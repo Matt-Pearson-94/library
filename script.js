5918a96c32d912f026a52ad039a7bbae18f8044d
Your Name <mattpearson94@gmail.com>
2023-02-03 11:53:40 +0000

    created the layout

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("test")
+let myLibrary = [];
+
+function Book(author, title, pages, isRead) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+const book1 = new Book('J.K. Rowling', 'Harry Potter and the Deathly Hallows', 501, true)
+const book2 = new Book('Stephen King', 'The Shining', 253, false)
+
+function addBookToLibrary() {
+    console.log(myLibrary)
+}
+
+// console.log(book1)
+// myLibrary.push(book1)
+// myLibrary.push(book2)
+// console.log(myLibrary)
+
+// console.log(myLibrary[0])
+
+// const button = document.querySelector('.add-book')
+
+const main = document.querySelector('.main')
+
+document.querySelector('.add-book').addEventListener("click", getBook)
+
+function getBook() {
+    alert('hello world')
+}
+
