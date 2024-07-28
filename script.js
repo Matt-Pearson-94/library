66a92a6835100364ae3e43afbcb44b2cccc4ea96
Your Name <mattpearson94@gmail.com>
2024-07-28 21:30:25 +0100

    Added the logo, and some styling

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-class newBook {
-    constructor(name, author, numOfPages, isRead) {
-        this.name = name
-        this.author = author
-        this.numOfPages = numOfPages
-        this.isRead = isRead
-    }
+const mainContents = document.querySelector('.main-contents-container')
+
+
+const myLibrary = [
+    {
+        author: 'J.K. Rowling',
+        title: 'Harry Potter and the Deathly Hallows',
+        numberOfPages: '500',
+        haveRead: true
+    }, {
+        author: 'Stephen King',
+        title: 'The Shining',
+        numberOfPages: '250',
+        haveRead: false
+    }, {
+        author: 'Michael Scott',
+        title: 'Threat Level Midnight',
+        numberOfPages: '350',
+        haveRead: true
+    },
+];
+
+function Book() {
+  // the constructor...
 }
 
-const book1 = new newBook('Harry Potter', 'J.K Rowling', 325, 'Yes')
+function addBookToLibrary() {
+  // do stuff here
+}
 
-console.log(book1)
+myLibrary.forEach(item => {
+    const newCard = document.createElement('div')
+    const authorName = document.createElement('div')
+    const bookTitle = document.createElement('div')
+    // const  = document.createElement('div')
+    newCard.classList.add('book-card')
 
-const form = document.querySelector('.form')
-const addBook = document.querySelector('.addbookbutton')
 
-addBook.addEventListener('click', event => {
-    form.style.display = 'block'
+    mainContents.appendChild(newCard)
 })
+
+
+// addBook()
