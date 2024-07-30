5e1da94457b2d3ca0d9803e6cb77e7550519b468
Matt Pearson <mattpearson94@gmail.com>
2024-07-30 11:40:34 +0100

    Added the pop up burtton to add book

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const mainContents = document.querySelector('.main-contents-container')
-
+const popupContainer = document.querySelector('.pop-up-container')
+const popupBlockBackground = document.querySelector('.pop-up-background') 
+const closeButton = document.querySelector('.close')
 
 const myLibrary = [
     {
@@ -28,16 +30,44 @@ function addBookToLibrary() {
   // do stuff here
 }
 
-myLibrary.forEach(item => {
-    const newCard = document.createElement('div')
-    const authorName = document.createElement('div')
-    const bookTitle = document.createElement('div')
-    // const  = document.createElement('div')
-    newCard.classList.add('book-card')
+function createBook() {
+    myLibrary.forEach(item => {
+        const newCard = document.createElement('div')
+        const authorName = document.createElement('div')
+        const bookTitle = document.createElement('div')
+        const numPages = document.createElement('div')
+        const tickbox = document.createElement('input')
+        const tickboxDes = document.createElement('label')
+        newCard.classList.add('book-card')
+        authorName.textContent = `Author: ${item.author}`
+        bookTitle.textContent = `Title: ${item.title}`
+        numPages.textContent = `Number of pages: ${item.numberOfPages}`
+        tickbox.setAttribute('id', 'have-read')
+        tickbox.type = 'checkbox'
+        tickboxDes.setAttribute('for', 'have-read')
+        tickboxDes.textContent = 'Have read: '
+        mainContents.appendChild(newCard)
+        newCard.appendChild(authorName)
+        newCard.appendChild(bookTitle)
+        newCard.appendChild(numPages)
+        newCard.appendChild(tickboxDes)
+        newCard.appendChild(tickbox)
+    })
+}
 
+function addBook() {
+    popupContainer.style.display = 'block'
+    popupBlockBackground.style.display = 'block'
+}
 
-    mainContents.appendChild(newCard)
+closeButton.addEventListener('click', close => {
+    popupContainer.style.display = 'none'
+    popupBlockBackground.style.display = 'none'
 })
 
 
-// addBook()
+function removeBook() {
+    myLibrary.splice(0, 1)
+    console.log(myLibrary)
+    createBook()
+}
